0879750613ca55c4df3c6b28889daaaaff2aaa04
xfl <xfl@ncmem.com>
2017-05-20 17:14:56 +0800

    增加js引用

--- index.php
+++ index.php
@@ -7,7 +7,10 @@
     <link type="text/css" rel="Stylesheet" href="WordPaster/js/skygqbox.css" />
     <script type="text/javascript" src="WordPaster/js/json2.min.js" charset="utf-8"></script>
     <script type="text/javascript" src="WordPaster/js/jquery-1.4.min.js" charset="utf-8"></script>
-    <script type="text/javascript" src="WordPaster/js/WordPaster.js" charset="utf-8"></script>
+    <script type="text/javascript" src="WordPaster/js/w.edge.js" charset="utf-8"></script>
+    <script type="text/javascript" src="WordPaster/js/w.app.js" charset="utf-8"></script>
+    <script type="text/javascript" src="WordPaster/js/w.file.js" charset="utf-8"></script>
+	<script type="text/javascript" src="WordPaster/js/WordPaster.js" charset="utf-8"></script>
     <script type="text/javascript" src="WordPaster/js/skygqbox.js" charset="utf-8"></script>
 	<script type="text/javascript" src="tiny_mce/tiny_mce.js"></script>
 </head>
